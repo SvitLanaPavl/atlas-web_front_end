b7661d381a19b36f4af41fd16e382625d9f79b2d
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-03-08 11:24:40 -0600

    task 3 done - careful with variable type and loop end condition

--- Javascript_advanced/1-nested_functions.js
+++ Javascript_advanced/1-nested_functions.js
@@ -1,15 +1,15 @@
 let globalVariable = 'Welcome';
 function outer() {
-    alert(globalVariable);
-    let course = 'Holberton';
-    function inner() {
-        alert(`${globalVariable} ${course}`);
-        let exclamation = '!';
-        function inception() {
-            alert(`${globalVariable} ${course}${exclamation}`)
-        }
-        inception();
+  alert(globalVariable);
+  let course = 'Holberton';
+  function inner() {
+    alert(`${globalVariable} ${course}`);
+    let exclamation = '!';
+    function inception() {
+      alert(`${globalVariable} ${course}${exclamation}`)
     }
-    inner();
+    inception();
+  }
+  inner();
 }
 outer();
--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -1,7 +1,7 @@
 function welcomeMessage(fullName) {
-    return function() {
-         alert(`Welcome ${fullName}`);
-    }
+  return function() {
+    alert(`Welcome ${fullName}`);
+  }
 }
 var guillaume = welcomeMessage('Guillaume');
 var alex = welcomeMessage('Alex');
